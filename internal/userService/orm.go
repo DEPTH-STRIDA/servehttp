8577a1374a97239231d19e99cc524875388e2fba
DEPTH-STRIDA <awesome.gail@yandex.ru>
2025-01-26 22:48:13 +0300

    user handlers (5 пункт) + пакет userService

--- internal/userService/orm.go
+++ internal/userService/orm.go
@@ -1,1 +1,19 @@
 package userService
+
+import (
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Tasks    []Task `json:"tasks"`
+}
+
+type Task struct {
+	gorm.Model
+	Task   string `json:"task"`
+	IsDone bool   `json:"is_done"`
+	UserID uint   `json:"user_id"`
+}
